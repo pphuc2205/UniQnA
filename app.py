1a9a993de749524ee60a458cb656c7b8cf210bc1
PhvcsLep <phamphuc2k22605@gmail.com>
2023-10-13 11:37:51 +0700

    new update

--- app.py
+++ app.py
@@ -91,7 +91,6 @@ def main():
 
                 # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
-                st.write(text_chunks)
 
                 # create vector store
                 vectorstore = get_vectorstore(text_chunks)
--- htmlTemplates.py
+++ htmlTemplates.py
@@ -10,16 +10,10 @@ css = '''
     background-color: #475063
 }
 .chat-message .avatar {
-  width: 20%;
-}
-.chat-message .avatar img {
-  max-width: 78px;
-  max-height: 78px;
-  border-radius: 50%;
-  object-fit: cover;
+  width: 10%;
 }
 .chat-message .message {
-  width: 80%;
+  width: 90%;
   padding: 0 1.5rem;
   color: #fff;
 }
@@ -28,7 +22,9 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="./img/robot-solid.svg">
+        <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512">
+            <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+        </svg>
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +33,9 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="./img/user-solid.svg">
+        <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 640 512">
+            <path d="M320 0c17.7 0 32 14.3 32 32V96H472c39.8 0 72 32.2 72 72V440c0 39.8-32.2 72-72 72H168c-39.8 0-72-32.2-72-72V168c0-39.8 32.2-72 72-72H288V32c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H208zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H304zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H400zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224H64V416H48c-26.5 0-48-21.5-48-48V272c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H576V224h16z"/>
+        </svg>
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
