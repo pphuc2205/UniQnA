b1e58111b06cec888ac01e8046ec2d49ef05edac
PhvcsLep <phamphuc2k22605@gmail.com>
2023-10-16 21:45:18 +0700

    new update 16/10/2023

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
@@ -32,7 +31,6 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    #embeddings = OpenAIEmbeddings()
     embeddings = HuggingFaceInstructEmbeddings(
         model_name="thenlper/gte-large")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
@@ -40,7 +38,6 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
-    #llm = ChatOpenAI()
     llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
 
     memory = ConversationBufferMemory(
