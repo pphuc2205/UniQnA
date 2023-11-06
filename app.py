35091a6703300436a8de28e286d1c4d3ef09d023
PhvcsLep <phamphuc2k22605@gmail.com>
2023-11-06 20:16:04 +0700

    new update 6-11-2023

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
@@ -22,49 +25,60 @@ def get_pdf_text(pdf_docs):
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
+        chunk_size=500,
+        chunk_overlap=50,
         length_function=len
     )
+    # text_splitter = RecursiveCharacterTextSplitter(
+    #     chunk_size=200, 
+    #     chunk_overlap=20, 
+    #     length_function=len
+    # )
     chunks = text_splitter.split_text(text)
     return chunks
 
 
 def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceInstructEmbeddings(
-        model_name="thenlper/gte-large")
+    embeddings = HuggingFaceInstructEmbeddings(model_name="intfloat/multilingual-e5-base")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature": 0.5, "max_length": 512})
+    llm = HuggingFaceHub(
+        repo_id="bigscience/bloom",
+        model_kwargs={"temperature": 0.5, "max_length": 512}
+    )
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
+        llm=llm, 
+        retriever=vectorstore.as_retriever(), 
         memory=memory
     )
     return conversation_chain
 
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+    response = st.session_state.conversation({"question": user_question})
+    # st.write(response)
+    st.session_state.chat_history = response["chat_history"]
 
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(
+                user_template.replace("{{MSG}}", message.content),
+                unsafe_allow_html=True,
+            )
         else:
-            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+            st.write(
+                bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True
+            )
 
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="UniQnA",
-                       page_icon=":books:")
+    st.set_page_config(page_title="UniQnA", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -80,7 +94,8 @@ def main():
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True
+        )
         if st.button("Process"):
             with st.spinner("Processing"):
                 # get pdf text
@@ -96,5 +111,5 @@ def main():
                 st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
